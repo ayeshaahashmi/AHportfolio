25c86d1d63ee2b5c5dbd5fce3dec46c325204cd5
ayeshaahashmi <ayeshaahashmi4@gmail.com>
2024-01-27 12:59:36 +0500

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Lesgo from './components/lesgo.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Lesgo></Lesgo>
+    </div>
+  );
+}
+
+export default App;
